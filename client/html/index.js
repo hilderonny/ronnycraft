e785ed8a315770c5ca63d6aa6ce419a090b2b434
hilderonny <github@hildebrandt2014.de>
2017-02-25 21:58:06 +0100

    Created small default flat demo

--- client/html/index.js
+++ client/html/index.js
@@ -1,3 +1,15 @@
-var createGame = require('voxel-hello-world');
+var voxelEngine = require('voxel-engine');
+var voxelPlayer = require('voxel-player');
 
-var game = createGame();
+var game = voxelEngine({
+    materials: ['grass'],
+    generate: function(x, y, z) {
+        return y === 0 ? 1 : 0;
+    }
+});
+
+game.appendTo(document.body);
+
+var avatar = voxelPlayer(game)('texture/player.png');
+avatar.possess();
+avatar.yaw.position.set(0, 1, 4);
