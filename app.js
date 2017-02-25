7d3ab54ca77bc70f9955951eef52e48963a73f9a
hilderonny <github@hildebrandt2014.de>
2017-02-25 20:02:33 +0100

    Included VoxelJS with Hello World demo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var browserify = require('browserify')();
+var fs = require('fs');
 
 var app = express();
 var port = process.env.PORT || 5000;
@@ -9,6 +11,11 @@ var dbUri = process.env.MONGODB_URI || 'mongodb://localhost/ronnycraft';
 // Initialize database connection before starting the app
 MongoClient.connect(dbUri, (err, db) => {
     if(err) throw err;
+
+    // Generate client side includes
+    browserify.add(__dirname + '/client/html/index.js');
+    browserify.bundle().pipe(fs.createWriteStream(__dirname + '/client/html/generated.js'));
+
     // Store db connection in request object req.db
     app.use((req, res, next) => {
         req.db = db;
@@ -19,7 +26,7 @@ MongoClient.connect(dbUri, (err, db) => {
 
     // Server static content
     app.use(express.static(__dirname + '/client/html'));
-    
+
     // Start application
     app.listen(port, function() {
         console.log(`Application is running on port ${port}`);
