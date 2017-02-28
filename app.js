16a1ce2eed57e81eeb1f911de5398de038760675
hilderonny <github@hildebrandt2014.de>
2017-02-28 23:08:10 +0100

    Replaced cache with database.

--- app.js
+++ app.js
@@ -10,73 +10,86 @@ var server = http.createServer(app);
 var port = process.env.PORT || 5000;
 var dbUri = process.env.MONGODB_URI || 'mongodb://localhost/ronnycraft';
 
-// Cache chunks
-var chunkCache = {};
-
 // Generate chunks
-var loadChunk = (position) => {
+var loadChunk = (db, position, callback) => {
     var chunkId = position.join('|');
-    if (chunkCache[chunkId]) {
-        return chunkCache[chunkId];
-    }
-    var chunk = {
-        position:position,
-        voxels:new Int8Array(32*32*32),
-        dims:[32,32,32]
-    }
-    var y32 = position[1] * 32;
-    for (vz = 0, n = 0; vz < 32; ++vz) {
-        for (vy = 0, y_vy = y32; vy < 32; ++vy, ++y_vy) {
-            for (vx = 0; vx < 32; ++vx, ++n) {
-                if (y_vy < 0) {
-                    chunk.voxels[n] = 2;
-                } else if (y_vy === 0) {
-                    chunk.voxels[n] = 1;
+    var collection = db.collection('chunks');
+    collection.findOne({chunkId:chunkId}, (err, chunk) => {
+        if (!chunk) {
+            var chunk = {
+                chunkId:chunkId,
+                position:position,
+                voxels:new Int8Array(32*32*32),
+                dims:[32,32,32]
+            }
+            var y32 = position[1] * 32;
+            for (vz = 0, n = 0; vz < 32; ++vz) {
+                for (vy = 0, y_vy = y32; vy < 32; ++vy, ++y_vy) {
+                    for (vx = 0; vx < 32; ++vx, ++n) {
+                        if (y_vy < 0) {
+                            chunk.voxels[n] = 2;
+                        } else if (y_vy === 0) {
+                            chunk.voxels[n] = 1;
+                        }
+                    }
                 }
             }
+            collection.insert(chunk, {fullResult:true}, (err, insertedChunk) => {
+                callback(collection, insertedChunk);
+            });
+        } else {
+            callback(collection, chunk);
         }
-    }
-    chunkCache[chunkId] = chunk;
-    return chunk;
+    });
 };
 
 // Handle chunk manipulation
-var createBlock = (position) => {
-    setBlock(position, 2);
+var createBlock = (db, position, callback) => {
+    setBlock(db, position, 2, callback);
 };
-var removeBlock = (position) => {
-    setBlock(position, 0);
+var removeBlock = (db, position, callback) => {
+    setBlock(db, position, 0, callback);
 };
-var setBlock = (position, type) => {
+var setBlock = (db, position, type, callback) => {
     var chunkPosition = [Math.floor(position[0] / 32), Math.floor(position[1] / 32), Math.floor(position[2] / 32)];
-    var chunk = loadChunk(chunkPosition);
-    var x = position[0] - chunkPosition[0]*32;
-    var y = position[1] - chunkPosition[1]*32;
-    var z = position[2] - chunkPosition[2]*32;
-    var n = (z*32+y)*32+x;
-    chunk.voxels[n] = type;
-}
-
-// Initialize websockets
-var io = socketIo(server);
-io.on('connection', (socket) => {
-    socket.on('msg', (message) => {
-        switch(message.type) {
-            case 'create': createBlock(message.position); break;
-            case 'remove': removeBlock(message.position); break;
-        }
-        io.emit('msg', message); // Also send back to sender so that the sender can also handle block creation / deletion
+    loadChunk(db, chunkPosition, (collection, chunk) => {
+        var x = position[0] - chunkPosition[0]*32;
+        var y = position[1] - chunkPosition[1]*32;
+        var z = position[2] - chunkPosition[2]*32;
+        var n = (z*32+y)*32+x;
+        chunk.voxels[n] = type;
+        collection.update({_id:chunk._id}, {$set:{voxels:chunk.voxels}}, (err, updatedChunk) => {
+            callback(updatedChunk);
+        });
     });
-    socket.on('loadChunk', (position) => {
-        var chunk = loadChunk(position);
-        socket.emit('loadChunk', chunk);
-    })
-});
+}
 
 // http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connection-pooling
 // Initialize database connection before starting the app
 MongoClient.connect(dbUri, (err, db) => {
     if(err) throw err;
+
+    // Initialize collections, let it do in the background and do not wait for completion
+    db.createCollection('chunks');
+
+    // Initialize websockets
+    var io = socketIo(server);
+    io.on('connection', (socket) => {
+        socket.on('msg', (message) => {
+            var callback = () => {
+                io.emit('msg', message); // Also send back to sender so that the sender can also handle block creation / deletion
+            };
+            switch(message.type) {
+                case 'create': createBlock(db, message.position, callback); break;
+                case 'remove': removeBlock(db, message.position, callback); break;
+            }
+        });
+        socket.on('loadChunk', (position) => {
+            var chunk = loadChunk(db, position, (collection, chunk) => {
+                socket.emit('loadChunk', chunk);
+            });
+        })
+    });
 
     // Generate client side includes
     console.log(`Generating client side code`);
